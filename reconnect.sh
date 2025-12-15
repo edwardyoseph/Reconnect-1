ebcda81c5eca49fcb66b5ba4f273c035c0d56013
edwardyoseph <116825212+edwardyoseph@users.noreply.github.com>
2025-12-15 08:34:36 +0700

    Update reconnect.sh

--- reconnect.sh
+++ reconnect.sh
@@ -4,9 +4,6 @@ LOG_FILE="/sdcard/log.txt"   # Lokasi file log
 ACT="com.roblox.client.startup.ActivitySplash"
 
 while true; do
-    echo "SCANNING DATA"
-
-    # Mengambil blok per bot dari file log
     awk -v RS="--------------------------------------------------" '
     NF > 0 {
         username="";
